f7696d73216c4e16f8dd8e5f742c42374560c5ad
linh8911nt <thanhlinh12314@gmail.com>
2018-03-20 15:34:56 +0700

    pf-java-display-geometry

---
+++ DisplayGeometry.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+public class DisplayGeometry {
+    public static void main(String[] args) {
+        int choice;
+        Scanner input = new Scanner(System.in);
+
+        do {
+            System.out.println("Menu");
+            System.out.println("1. Vẽ hình chữ nhật");
+            System.out.println("2. Vẽ hình tam giác vuông, có cạnh góc vuông ở bottom-left");
+            System.out.println("3. Vẽ hình tam giác vuông, có cạnh góc vuông ở top-left");
+            System.out.println("4. Vẽ hình tam giác cân");
+            System.out.println("0. Exit");
+            System.out.println("Nhập lựa chọn: ");
+            choice = input.nextInt();
+
+            int i, j;
+
+            switch (choice){
+                case 1:
+                    for (i = 0; i < 4; i++){
+                        for (j = 0; j < 5; j++) {
+                            System.out.printf("*");
+                        }
+                        System.out.println();
+                    }
+                    break;
+                case 2:
+                    for (i = 0; i < 7; i++){
+                        for (j = 0; j < i; j++){
+                            System.out.printf("*");
+                        }
+                        System.out.println();
+                    }
+                    break;
+                case 3:
+                    for (i = 8; i >= 1; i--){
+                        for (j = 0; j < i; j++){
+                            System.out.printf("*");
+                        }
+                        System.out.println();
+                    }
+                    break;
+                case 4:
+                    for (i = 0; i < 5; i++){
+                        for (j = 0; j < i; j++){
+                            System.out.printf("*");
+                        }
+                        for (int k = 1; k < j; k++){
+                            System.out.printf("*");
+                        }
+                        System.out.println();
+                    }
+                    break;
+                case 0:
+                    System.exit(0);
+                default:
+                    System.out.println("No choice!");
+            }
+
+        }while (choice != 0);
+    }
+}
