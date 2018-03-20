080e6b64740ffba2dd91c7c2c39c8dfabf4b9554
linh8911nt <thanhlinh12314@gmail.com>
2018-03-20 16:56:05 +0700

    pf-java-display-geometry

--- DisplayGeometry.java
+++ DisplayGeometry.java
@@ -10,7 +10,9 @@ public class DisplayGeometry {
             System.out.println("1. Vẽ hình chữ nhật");
             System.out.println("2. Vẽ hình tam giác vuông, có cạnh góc vuông ở bottom-left");
             System.out.println("3. Vẽ hình tam giác vuông, có cạnh góc vuông ở top-left");
-            System.out.println("4. Vẽ hình tam giác cân");
+            System.out.println("4. Vẽ hình tam giác vuông, có cạnh góc vuông ở bottom-right");
+            System.out.println("5. Vẽ hình tam giác vuông, có cạnh góc vuông ở top-right");
+            System.out.println("6. Vẽ hình tam giác cân");
             System.out.println("0. Exit");
             System.out.println("Nhập lựa chọn: ");
             choice = input.nextInt();
@@ -43,6 +45,25 @@ public class DisplayGeometry {
                     }
                     break;
                 case 4:
+                    for (i = 0; i < 7; i++){
+                        for (j = 7; j > i; j--){
+                            System.out.printf("*");
+                        }
+                        System.out.println();
+                    }
+                    break;
+                case 5:
+                    for (i = 0; i < 8 ; i++){
+                        for (j = 0; j < 8; j++){
+                            if (j >= i)
+                                System.out.printf("*");
+                            else
+                                System.out.printf(" ");
+                        }
+                        System.out.println();
+                    }
+                    break;
+                case 6:
                     for (i = 0; i < 5; i++){
                         for (j = 0; j < i; j++){
                             System.out.printf("*");
